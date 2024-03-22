1ebee273fb3c42d81dda7751193dc9167c7e8a04
Marco Carvalho <maacnet@hotmail.com>
2024-03-21 21:47:49 -0300

    null

--- Microservices/IdentityAPI/Models/Usuario.cs
+++ Microservices/IdentityAPI/Models/Usuario.cs
@@ -2,7 +2,7 @@
 {
     public class Usuario
     {
-        public string NomeUsuario { get; set; }
-        public string Senha { get; set; }
+        public string? NomeUsuario { get; set; }
+        public string? Senha { get; set; }
     }
 }
